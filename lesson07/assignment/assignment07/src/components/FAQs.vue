8a97f29894c078c90d03fda0d4046fbed3a11aae
elena <ejdon@uw.edu>
2020-05-31 16:25:13 -0700

    starting assignment

---
+++ lesson07/assignment/assignment07/src/components/FAQs.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+<!-- FAQs -->
+<div class="container">
+  <h2 class="m-3">Frequently asked questions</h2>
+  <div class="accordion m-3">
+    <div class="card">
+      <!--Question-->
+      <div class="card-header">
+        <h2>
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne">
+          Will I be asked to participate?
+          </button>
+        </h2>
+      </div>
+      <!--Answer-->
+      <div id="collapseOne" class="collapse show" data-parent="#accordion">
+        <div class="card-body">
+          A car seat clinic is an educational opportunity. We want you to get the most value for your time, so plan to participate in the check-up process.        </div>
+      </div>
+    </div>
+    <!--Question-->
+    <div class="card">
+      <div class="card-header">
+        <h2>
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo">
+          Should I bring anything?
+          </button>
+        </h2>
+      </div>
+      <!--Second collapsible content-->
+      <div id="collapseTwo" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Yes. Bring the owner's manuals for the car seat and vehicle. Having diapers, snacks and toys handy may make waiting time more pleasant.
+        </div>
+      </div>
+    </div>
+    <!--Question-->
+    <div class="card">
+      <div class="card-header">
+        <h2>
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree">
+          Will there be a place for kids to play?
+          </button>
+        </h2>
+      </div>
+      <!--Third collapsible content-->
+      <div id="collapseThree" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          There will be a small play area for kids. However, we recommend bringing personal toys as well.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'FAQs',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ lesson07/assignment/assignment07/src/components/images.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+<!-- alert -->
+ <div class="alert alert-warning">To continue to serve child caregivers during social distancing, CSK has formed a virtual checkup sub-team. If you are a currently certified child passenger safety technician who would like to be added to our team, please contact Jane at jane.doe@mail.com.</div>
+ <main>
+ <!-- image gallery -->
+ <div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+        <img src="../assets/sidebar-1.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <p>CSK will teach you how to install your child's car seat.</p>
+        </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+        <img src="../assets/sidebar-2.jpg" alt="Nature" style="width:100%">
+        <div class="caption">
+          <p>CSK can assist families with babies, expectant parents, and others, including grandparents and nannies.</p>
+        </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+        <img src="../assets/sidebar-3.jpg" alt="Fjords" style="width:100%">
+        <div class="caption">
+          <p>CSK technicians will ensure your baby's care seat harness is used properly.</p>
+        </div>
+    </div>
+  </div>
+</div>
+</main>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'images',
+  props: {
+    msg: String
+  }
+}
+</script>
